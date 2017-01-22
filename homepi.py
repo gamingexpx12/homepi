f0883b5b05b022417a83d3828be2ca622a0f8cea
gamingexpx12 <gamingexpx12@gmail.com>
2017-01-22 15:31:15 +0100

    Added rgb lcd

--- homepi.py
+++ homepi.py
@@ -1,10 +1,12 @@
 # Made in gamingexpx12, not china.
 
 from grovepi import *
+from grove_rgb_lcd import *
 import time
 
 # Pref
 dhtsensor = 7 # DI pin with PWM
+# display is ic2 based
 
 pinMode(dhtsensor, "input")
 out = ""
@@ -14,6 +16,8 @@ while True:
     try:
         t, hum = dht(dhtsensor, 0)
         out = "It's {}*C degrees and {} percent humidity".format(t,hum)
+        time.sleep(0.1)
+        setText = out
         print(out)
         time.sleep(1)
     except KeyboardInterrupt:
